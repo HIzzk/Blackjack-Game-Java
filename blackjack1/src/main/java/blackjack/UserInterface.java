3eb074daa62b5d8f0b926f18daa254bfdd891bbe
HenryIsaac252 <115743205+HenryIsaac252@users.noreply.github.com>
2022-10-20 17:24:02 -0400

    Add files via upload

--- blackjack1/src/main/java/blackjack/UserInterface.java
+++ blackjack1/src/main/java/blackjack/UserInterface.java
@@ -1,13 +1,49 @@
 package blackjack;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
+    Scanner myScanner = new Scanner(System.in);
     Deck theDeck = new Deck();
 
     public void makeFirstDeck() {
         makeDeck(theDeck);
     }
 
+    public void welcomeMenu() {
+        while (true) {
+            System.out.println("Welcome to the BlackJack App!");
+            System.out.println("Would you like to play?");
+            System.out.println();
+            System.out.println("1) Play new game");
+            System.out.println("2) Quit");
+            String userChoice = myScanner.nextLine();
+            try {
+                int userInt = Integer.parseInt(userChoice);
+                switch (userInt) {
+                    case 1:
+                        setupGame();
+                        break;
+                    case 2:
+                        System.exit(0);
+                }
+
+            } catch (NumberFormatException e) {
+
+            }
+        }
+    }
+
+    public void setupGame() {
+        while (true) {
+            System.out.println("New Game");
+            System.out.println();
+            System.out.println("Set initial cash");
+            System.out.println("Set bet amount");
+            System.out.println("Return to main menu");
+        }
+    }
 
     public void makeDeck(Deck testDeck) {
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
